2d2ce232c93af0a6eb4abd13b31cfa5877e116b5
Mariano Conti <nanexcool@gmail.com>
2015-01-28 01:24:12 -0300

    Basic Movement

---
+++ Movement/Movement/Game1.cs
@@ -0,0 +1,133 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Movement
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+
+        Player player;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            //IsFixedTimeStep = false;
+            //graphics.SynchronizeWithVerticalRetrace = false;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            font = Content.Load<SpriteFont>("Font");
+
+            player = new Player(Content.Load<Texture2D>("batman"), new Vector2(100, 300));
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            
+            KeyboardState state = Keyboard.GetState();
+
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            Vector2 axis = Vector2.Zero;
+
+            if (state.IsKeyDown(Keys.A))
+            {
+                axis.X = -1;
+            }
+            else if (state.IsKeyDown(Keys.E))
+            {
+                axis.X = 1;
+            }
+
+            if (state.IsKeyDown(Keys.Space))
+            {
+                player.Jump();
+            }
+
+            player.Move(axis);
+
+            player.Update(elapsed);
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp, null, null);
+
+            player.Draw(spriteBatch);
+
+            spriteBatch.End();
+
+            spriteBatch.Begin();
+
+            spriteBatch.DrawString(font, player.Position.ToString(), new Vector2(0, 0), Color.Black);
+            spriteBatch.DrawString(font, player.Velocity.ToString(), new Vector2(0, 20), Color.Black);
+            spriteBatch.DrawString(font, player.Acceleration.ToString(), new Vector2(0, 40), Color.Black);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Movement/Movement/Player.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Movement
+{
+    class Player
+    {
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public Vector2 Acceleration;
+
+        float friction;
+        bool jumping = false;
+
+        public Texture2D Texture { get; set; }
+        public Color Color { get; set; }
+
+        Vector2 wantedPosition;
+
+        public Player(Texture2D texture, Vector2 position)
+        {
+            Texture = texture;
+            Position = position;
+            Color = Color.White;
+        }
+
+        public void Move(Vector2 axis)
+        {
+            wantedPosition = axis;
+        }
+
+        public void Update(float elapsed)
+        {
+            friction = -6.0f;
+
+            if (jumping)
+            {
+                friction = -6.0f;
+            }
+
+            Vector2 speed = new Vector2(600, 0);
+            
+            Acceleration = wantedPosition * speed;
+
+            Acceleration += friction * Velocity;
+
+            // Gravity
+            Acceleration.Y = 3000;
+
+            Vector2 delta = (0.5f * Acceleration * elapsed * elapsed + Velocity * elapsed);
+
+            Velocity += Acceleration * elapsed;
+
+            Position += delta;
+
+            if (Position.Y >= 300)
+            {
+                Position.Y = 300;
+                Velocity.Y = 0;
+                jumping = false;
+            }
+        }
+
+        public void Jump()
+        {
+            if (!jumping)
+            {
+                jumping = true;
+                Velocity.Y = -1000;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Texture, Position, Color);
+            //spriteBatch.Draw(Texture, Position, Color.Red * 0.7f);
+        }
+    }
+}
